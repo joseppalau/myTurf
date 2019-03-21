96c163479624a8e3ad07ff94c0eaeb068aa9fbb8
administrador <jeppalau83@gmail.com>
2019-03-21 18:43:57 +0100

    manufactuer, distributor, product and fertiliser added to models

--- turf_app/admin.py
+++ turf_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Manufacturer, Distributor, Fertiliser
 # Register your models here.
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
--- turf_app/models.py
+++ turf_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    address = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Distributor(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    address = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='products')
+    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE, related_name='products')
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Fertiliser(Product):
+    nitrogen = models.DecimalField(max_digits=10, decimal_places=3)
+    phosphorus = models.DecimalField(max_digits=10, decimal_places=3)
+    potassium = models.DecimalField(max_digits=10, decimal_places=3)
+    magnesium = models.DecimalField(max_digits=10, decimal_places=3)
+
+    def __str__(self):
+        return self.name
+
+
