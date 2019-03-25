8b6f74bb9a8bb4bfcfcdf6de982d45c253b5e094
administrador <jeppalau83@gmail.com>
2019-03-25 19:38:46 +0100

    market products logic and templates done

--- turf_app/urls.py
+++ turf_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    path('dashboard/', views.dashboard_page, name='dashboard_page')
+    path('dashboard/', views.dashboard_page, name='dashboard_page'),
+    path('products/', views.products_page, name='products_page')
 ]
 
--- turf_app/views.py
+++ turf_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Fertiliser
+from .models import Fertiliser, FertiliserUser
 
 
 def landing_page(request):
@@ -8,7 +8,22 @@ def landing_page(request):
 
 def dashboard_page(request):
     user = request.user
-    fertilisers = Fertiliser.objects
-    return render(request, 'turf_app/dashboard.html', {'user': user})
+    fertilisers = FertiliserUser.objects.all()
+    return render(request, 'turf_app/dashboard.html', {'user': user, 'fertilisers': fertilisers})
+
+
+def products_page(request):
+    fertilisers = Fertiliser.objects.all()
+    status = []
+    for f in fertilisers:
+        if len(f.used_by.filter(user=request.user)) > 0:
+            status.append(True)
+            print(True)
+        else:
+            status.append(False)
+            print(False)
+    return render(request, 'turf_app/products.html', {'fertilisers': fertilisers, 'status': status})
+
+
 
 # Create your views here.
