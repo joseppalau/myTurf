2d3a824157742a8e4d09e3ebdcf1c00039ad86b9
administrador <jeppalau83@gmail.com>
2019-04-03 14:15:09 +0200

    input volum quantity hide/show depending on select type option

--- turf_app/static/js/dashboard.js
+++ turf_app/static/js/dashboard.js
@@ -7,3 +7,18 @@ $('#add-btn-applications').on('click', function(){
 $('#modal-form-close').on('click', function(){
     $('#modal-form').hide()
 })
+
+/*APPLICATION FORM SCRIPT*/
+
+$('#type-dropdown').change(function(){
+    if($('#volumQuantity-div').css.display == 'none'){
+        $('#volumQuantity-div').show()
+        $('#volumQuantity-div').css.display ='block'
+        console.log($('#volumQuantity').css.display)
+    } else {
+        $('#volumQuantity-div').hide()
+        $('#volumQuantity-div').css.display ='none'
+        console.log($('#volumQuantity').css.display)
+    }
+})
+
