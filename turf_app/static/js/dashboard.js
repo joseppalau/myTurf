12162051b8bac8a72e8b2348685e7e83869c4965
administrador <jeppalau83@gmail.com>
2019-04-05 20:07:57 +0200

    add products options plus delete icon on each new product row

--- turf_app/static/js/dashboard.js
+++ turf_app/static/js/dashboard.js
@@ -14,11 +14,38 @@ $('#type-dropdown').change(function(){
     if($('#volumQuantity-div').css.display == 'none'){
         $('#volumQuantity-div').show()
         $('#volumQuantity-div').css.display ='block'
-        console.log($('#volumQuantity').css.display)
     } else {
         $('#volumQuantity-div').hide()
         $('#volumQuantity-div').css.display ='none'
-        console.log($('#volumQuantity').css.display)
     }
 })
+
+// Add delete products in the application form
+$('#btn-add-item').on('click', addProductSelectDiv)
+function addProductSelectDiv() {
+    if(('#btn-delete-item') != undefined){
+        $('#btn-delete-item').remove()
+    }
+    $('#btn-add-item').remove()
+    $('#product-selection').append('<div class="product-select-div">\n' +
+        '                                <label for="">Product:</label>\n' +
+        '                                <select name="product-dropdown" id="" style="width:150px;">\n' +
+        '                                </select>\n' +
+        '                                <label for="">Quantity</label>\n' +
+        '                                <input type="text">\n' +
+        '                                <img id="btn-add-item" class="btn-add-application" src="/static/images/mas.png" alt="" width="20" height="20">\n' +
+        '                                <img id="btn-delete-item" class="btn-delete-application" src="/static/images/delete.png" alt="" width="20" height="20">\n' +
+        '                            </div>')
+    newBtnListener('btn-add-item')
+}
+
+function deleteProductSelection(){
+    let arrayProducts = jQuery.makeArray($('.product-select-div'))
+
+}
+
+
+function newBtnListener(id){
+    document.getElementById(id).addEventListener('click', addProductSelectDiv)
+}
 
