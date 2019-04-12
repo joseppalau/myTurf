426c2b526a1ae232830edc727dc88c06a794bdfe
administrador <jeppalau83@gmail.com>
2019-04-12 17:43:52 +0200

    application for ready to be deal with data. all works fine

--- turf_app/static/js/dashboard.js
+++ turf_app/static/js/dashboard.js
@@ -1,56 +1,99 @@
-/*Open and close modal form */
+var countProducts = 1 //it will count products being added or deleted
+var productSelectDiv = jQuery.makeArray($('.product-select-div')) // it will save products
+
+//open form
 $('#add-btn-applications').on('click', function(){
     $('#modal-form').show()
 })
 
+//close form
 $('#modal-form-close').on('click', function(){
     $('#modal-form').hide()
+    if(countProducts > 1){
+        for (let i = countProducts - 1; i > 0; i--) {
+            productSelectDiv[i].remove()
+        }
+    }
+    productSelectDiv[0].append(addImg)
+    countProducts = 1
+    newBtnListener('btn-add-item')
+
 })
 
-/*APPLICATION FORM SCRIPT*/
-$('#type-dropdown').change(function(){
-    if($('#volumQuantity-div').css.display == 'none'){
+// img add btn to be added or removed from products div
+var addImg = document.createElement('img')
+    addImg.src ="/static/images/mas.png"
+    addImg.id = 'btn-add-item'
+    addImg.width = 20
+    addImg.height = 20
+    addImg.class = "btn-add-application"
+
+// add volum input if application type selected is liquid
+$('#type-dropdown').change(function() {
+    if ($('#volumQuantity-div').css.display == 'none') {
         $('#volumQuantity-div').show()
-        $('#volumQuantity-div').css.display ='block'
+        $('#volumQuantity-div').css.display = 'block'
     } else {
         $('#volumQuantity-div').hide()
-        $('#volumQuantity-div').css.display ='none'
+        $('#volumQuantity-div').css.display = 'none'
     }
+    typeSelect($('#product-select-1'))
+    if (countProducts > 1) {
+        for (let i = countProducts - 1; i > 0; i--) {
+           productSelectDiv[i].remove()
+        }
+        productSelectDiv[0].append(addImg)
+        newBtnListener('btn-add-item')
+    }
+    countProducts = 1
 })
 
-// Add delete products in the application form
+//change products availability depending on type application
+typeSelect($('#product-select-1'))
+
+function typeSelect(selector) {
+    selector.empty()
+    if ($('#type-dropdown').val() === 'Liquid') {
+        for (let i = 0; i < liquidFertilisersNames.length; i++) {
+            selector.append('<option value=\"' + liquidFertilisersIds[i] + '\">' + liquidFertilisersNames[i] + '</option>')
+        }
+    } else {
+        for (let i = 0; i < solidFertilisersNames.length; i++) {
+            selector.append('<option value=\"' + solidFertilisersIds[i] + '\">' + solidFertilisersNames[i] + '</option>')
+        }
+    }
+}
+
+// Add products in the application form
 $('#btn-add-item').on('click', addProductSelectDiv)
 function addProductSelectDiv() {
     if(('#btn-delete-item') != undefined){
         $('#btn-delete-item').remove()
     }
     $('#btn-add-item').remove()
+    countProducts++
+    console.log('countproducts:' + countProducts)
     $('#product-selection').append('<div class="product-select-div">\n' +
-        '                                <label for="">Product:</label>\n' +
-        '                                <select name="product-dropdown" id="" style="width:150px;">\n' +
+        '                                <label for="product-select-' + countProducts.toString() + '">Product:</label>\n' +
+        '                                <select name="product-dropdown-' + countProducts.toString() + '" id="product-select-' + countProducts.toString() + '" style="width:150px;">\n' +
         '                                </select>\n' +
         '                                <label for="">Quantity</label>\n' +
         '                                <input type="text">\n' +
         '                                <img id="btn-add-item" class="btn-add-application" src="/static/images/mas.png" alt="" width="20" height="20">\n' +
         '                                <img id="btn-delete-item" class="btn-delete-application" src="/static/images/delete.png" alt="" width="20" height="20">\n' +
         '                            </div>')
+    productSelectDiv = jQuery.makeArray($('.product-select-div'))
     newBtnListener('btn-add-item')
     newDeleteBtnListener('btn-delete-item')
+    console.log('product-select-' + countProducts.toString())
+    typeSelect($('#product-select-' + countProducts.toString()))
 }
 
+// delete products in the application form
 function deleteProductSelection(){
-    let list_psdg = jQuery.makeArray($('.product-select-div')) // list from product-select-div-group class
-    console.log(list_psdg.length)
-    $('.product-select-div')[list_psdg.length - 1].remove()
-    list_psdg = list_psdg.slice(0,list_psdg.length - 1)
-    console.log(list_psdg.length)
-
-    let addImg = document.createElement('img')
-    addImg.src ="/static/images/mas.png"
-    addImg.id = 'btn-add-item'
-    addImg.width = 20
-    addImg.height = 20
-    addImg.class = "btn-add-application"
+    productSelectDiv[countProducts - 1].remove()
+    countProducts--
+    console.log(countProducts)
 
     let addImgDel = document.createElement('img')
     addImgDel.src ="/static/images/delete.png"
@@ -58,11 +101,11 @@ function deleteProductSelection(){
     addImgDel.width = 20
     addImgDel.height = 20
     addImgDel.class = "btn-add-application"
-    $('.product-select-div')[list_psdg.length - 1].append(addImg)
+    productSelectDiv[countProducts - 1].append(addImg)
     newBtnListener('btn-add-item')
 
-    if(jQuery.makeArray($('.product-select-div')).length > 1) {
-        $('.product-select-div')[list_psdg.length - 1].append(addImgDel)
+    if(countProducts > 1) {
+        productSelectDiv[countProducts - 1].append(addImgDel)
         newDeleteBtnListener('btn-delete-item')
     }
 }
@@ -75,25 +118,7 @@ function newDeleteBtnListener(id){
     document.getElementById(id).addEventListener("click", deleteProductSelection)
 }
 
-// select options depending on either liquid or solid application
 
-
-
-$('#type-dropdown').change(function(){
-    var select = $('#product-select-1')
-    if($('#type-dropdown').val() == 'Liquid'){
-        for(let i = 0; i < liquidFertilisersNames.length; i++ ){
-            select.append('<option value=\"' + liquidFertilisersIds[i] + '\">' + liquidFertilisersNames[i] + '</option>')
-        }
-    } else{
-         for(let i = 0; i < solidFertilisersNames.length; i++ ){
-            select.append('<option value=\"' + solidFertilisersIds[i] + '\">' + solidFertilisersNames[i] + '</option>')
-        }
-    }
-})
-
-
-console.log(solidFertilisersNames.length)
 
 
 
