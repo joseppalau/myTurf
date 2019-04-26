b3961dfb4c2f0f59dbd996f1aa2ce33242e0bd1b
administrador <jeppalau83@gmail.com>
2019-04-26 18:46:00 +0200

    first web scraping done succesfully. 1o days forecast got

--- turf_app/weather.py
+++ turf_app/weather.py
@@ -8,7 +8,7 @@ def simple_get(url):
     try:
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
-                return resp.content
+                return resp
             else:
                 return None
     except RequestException as e:
@@ -25,3 +25,24 @@ def is_good_response(resp):
 
 def log_error(e):
     print(e)
+
+
+raw_html = get('https://weather.com/es-US/tiempo/10dias/l/SPXX0040:1:SP')
+
+html = BeautifulSoup(raw_html.content, 'html.parser')
+
+section = html.find(id="main-DailyForecast-1bbda948-59cc-4040-9a36-d9c1ed37a806")
+
+trs = section.find_all('tr')[1:10]
+
+for tr in trs:
+    for td in tr.find_all('td'):
+        print(td.getText(), end=' | ')
+    print()
+
+
+
+
+
+
+
